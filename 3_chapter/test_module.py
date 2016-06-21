176621ba1e1c63444cf3e3ca19d680879c8572c2
happyin3 <1165790450@qq.com>
2016-06-21 23:40:25 +0800

    建议29

--- 3_chapter/test_module.py
+++ 3_chapter/test_module.py
@@ -4,3 +4,10 @@
 a = 1
 b = 'a'
 print "testing module import"
+
+
+class Test(object):
+    def __init__(self, course=None):
+        if course is None:
+            course = []
+        self.course = course
